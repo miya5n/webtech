42cc79bd9f30c1ed941a5a754879d586b746bd57
miya5n <miya5n.blues@gmail.com>
2016-03-21 01:53:41 +0900

    tigからコミット初

---
+++ functional/gettingstarted/GettingStarted.scala
@@ -0,0 +1,14 @@
+package functional.gettingStarted
+
+object GettingStarted {
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean =
+      if (n >= as.length-1) true
+      else if (gt(as(n), as(n+1))) false
+      else go(n+1)
+
+    go(0)
+  }
+}
+
